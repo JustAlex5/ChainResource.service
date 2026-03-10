ed1397eba8eec21357d1ba3a4c8c96662c0b9744
Alex Kovalyov <alex.kovalyov@t-a9.com>
2026-03-10 08:26:54 +0200

    Add semaphore for thread safety in file read/write operations

--- ChainResource.service/DAL/FileSystemDal.cs
+++ ChainResource.service/DAL/FileSystemDal.cs
@@ -10,38 +10,64 @@ public class FileSystemDal<T> : IStorageDal<T>
     private readonly FileSystemConfiguration _fileSystemConfiguration;
     private readonly ILogger<FileSystemDal<T>> _logger;
     private TimeSpan _expiration;
+    private readonly SemaphoreSlim _semaphore = new SemaphoreSlim(1, 1);
 
     public FileSystemDal(IOptions<FileSystemConfiguration> fileSystemConfiguration, ILogger<FileSystemDal<T>> logger)
     {
         _fileSystemConfiguration = fileSystemConfiguration.Value;
         _logger = logger;
-        _expiration = _fileSystemConfiguration.TTLTimeSpan;    
+        _expiration = _fileSystemConfiguration.TTLTimeSpan;
     }
+
     public bool IsWritable { get; } = true;
 
     public async Task<(T value, bool IsValid)> TryReadAsync()
     {
-        if (!File.Exists(_fileSystemConfiguration.FilePath))
-            return  (default, false);
-        var lastWrite = File.GetLastWriteTimeUtc(_fileSystemConfiguration.FilePath);
-        if (DateTime.UtcNow - lastWrite >= _expiration)
-            return (default,false);
-        var json = await File.ReadAllTextAsync(_fileSystemConfiguration.FilePath);
-        var res = JsonSerializer.Deserialize<T>(json);
-        return (res, true);
-
-
-
+        await _semaphore.WaitAsync();
+        try
+        {
+            if (!File.Exists(_fileSystemConfiguration.FilePath))
+                return (default, false);
+            var lastWrite = File.GetLastWriteTimeUtc(_fileSystemConfiguration.FilePath);
+            if (DateTime.UtcNow - lastWrite >= _expiration)
+                return (default, false);
+            var json = await File.ReadAllTextAsync(_fileSystemConfiguration.FilePath);
+            var res = JsonSerializer.Deserialize<T>(json);
+            return (res, true);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Exception occurred while trying to read from file system.");
+            return (default, false);
+        }
+        finally
+        {
+            _semaphore.Release();
+        }
     }
 
     public async Task WrtiAsync(T value)
     {
-        var dir = Path.GetDirectoryName(_fileSystemConfiguration.FilePath);
-        if (!Directory.Exists(dir))
+        await _semaphore.WaitAsync();
+        try
         {
-            Directory.CreateDirectory(dir);
+            var dir = Path.GetDirectoryName(_fileSystemConfiguration.FilePath);
+            if (!Directory.Exists(dir))
+            {
+                Directory.CreateDirectory(dir);
+            }
+
+            var json = JsonSerializer.Serialize<T>(value);
+            await File.WriteAllTextAsync(_fileSystemConfiguration.FilePath, json);
         }
-        var json = JsonSerializer.Serialize<T>(value);
-        await File.WriteAllTextAsync(_fileSystemConfiguration.FilePath, json);
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Exception occurred while trying to write to file system.");
+            throw;
+        }
+        finally
+        {
+            _semaphore.Release();
+        }
     }
 }
